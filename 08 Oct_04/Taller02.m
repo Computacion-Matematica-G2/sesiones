201feb8d54eacd67e01a34ac88e84169f6094f51
faiverht <faiverht@gmail.com>
2019-10-27 19:12:46 -0500

    Actualización de sesiones a la fecha

---
+++ 08 Oct_04/Taller02.m
@@ -0,0 +1,12 @@
+clc;clear;
+n = 9;  % Cantidad de aleatorios a generar (para a y b)
+mx= 3;  % Valor máximo/mínimo para el rango de aleatorios
+Limite = 1; % Valor de límite a procesar.
+
+% a) Generar n cantidad de números aleatorios enteros en el rango [0,mx].
+%    (graficar con 'o')
+% b) Generar n cantidad de números aleatorios enteros en el rango [-mx,0].
+%    (graficar con 'o')
+% c) Trazar línea (graficar) los datos del arreglo a, pero, modificando los
+%    valores menores a Limite pasándolos al entero superior).
+%    (Graficar con plot sin modificación, línea interpolada)
